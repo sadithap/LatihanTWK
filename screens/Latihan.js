cc1bd4905948002d0496a4dc36a9b721c6ef9377
I Gede Ketut Saditha Putra <sadithaputra5@gmail.com>
2023-03-24 13:22:20 +0800

    latihan logic sistem done, but still static

--- screens/Latihan.js
+++ screens/Latihan.js
@@ -1,4 +1,5 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, } from "react-native";
+import * as Animatable from 'react-native-animatable'
 import React, { useState } from "react";
 import colors from "../constants/Colors";
 import formSoal from "../constants/FormSoal";
@@ -6,52 +7,77 @@ import PrimaryButton from "../components/PrimaryButton";
 import OptionButton from "../components/OptionButton";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-const Latihan = () => {
-  const [score, setScore] = useState(0);
+const Latihan = ({navigation}) => {
+  const [showWrong, setShowWrong] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [respon, setRespon] = useState("");
+  const [respon, setRespon] = useState([]);
 
   function answerHandler(answer) {
-    setRespon(answer);
-  }
-
-  function nextHandler(){
-    if (respon === formSoal[currentIndex].jawaban) {
-      setScore(score + 1);
+    if (currentIndex !== 0) {
+      if (currentIndex === respon.length - 1) {
+        setRespon((prevRespon) => {
+          const tempRespon = [...prevRespon];
+          tempRespon[tempRespon.length - 1] = answer;
+          return tempRespon;
+        });
+      } else {
+        setRespon((prevRespon) => {
+          return [...prevRespon, answer];
+        });
+      }
+    } else {
+      setRespon([answer]);
     }
-    setCurrentIndex(currentIndex + 1);
   }
 
-  function finishHandler() {
-    alert(`Your score : ${score}`);
-    setCurrentIndex(0);
-    setScore(0);
+  function nextHandler() {
+    if (respon[currentIndex] === formSoal[currentIndex].jawaban) {
+      setCurrentIndex(currentIndex + 1);
+    } else {
+      setRespon((prevRespon) => {
+        const tempRespon = [...prevRespon];
+        tempRespon.pop();
+        return tempRespon;
+      });
+      this.shakeView.shake(800);
+      setShowWrong(true);
+      setTimeout(() => {
+        setShowWrong(false);
+      }, 2000);
+    }
   }
 
   return (
     <SafeAreaView style={styles.rootContainer}>
-      <View style={styles.container}>
+      <Animatable.View ref={(ref) => (this.shakeView = ref)} style={styles.container}>
         <Text style={styles.question}>{formSoal[currentIndex].soal}</Text>
         {console.log(formSoal[currentIndex].pilgan)}
         {formSoal[currentIndex].pilgan.map((option, index) => (
           <OptionButton
             key={index}
-            respon={respon}
+            respon={respon[currentIndex]}
             onpressed={() => answerHandler(option)}
           >
             {option}
           </OptionButton>
         ))}
         <View style={styles.changeQuestion}>
-          <View>
-          
-          </View>
-          <View>
-          {currentIndex!==formSoal.length-1 && <PrimaryButton onpressed={nextHandler} >Next</PrimaryButton>}
-          {currentIndex===formSoal.length-1 && <PrimaryButton onpressed={finishHandler}>Finish</PrimaryButton> }
+          <View></View>
+          <View style={styles.changeQuestion}>
+            <View>
+              {showWrong && <Text style={styles.wrongSign}>Salah</Text>}
+            </View>
+            <View>
+              {currentIndex !== formSoal.length - 1 && (
+                <PrimaryButton onpressed={nextHandler}>Next</PrimaryButton>
+              )}
+              {currentIndex === formSoal.length - 1 && (
+                <PrimaryButton onpressed={() => navigation.navigate("Hasil")}>Finish</PrimaryButton>
+              )}
+            </View>
           </View>
         </View>
-      </View>
+      </Animatable.View>
     </SafeAreaView>
   );
 };
@@ -77,5 +103,12 @@ const styles = StyleSheet.create({
     marginBottom: 5,
     flexDirection: "row",
     justifyContent: "space-between",
+  },
+  wrongSign: {
+    fontSize: 20,
+    borderRadius: 10,
+    padding: 10,
+    backgroundColor: "#990000",
+    color: colors.primary4,
   },
 });
